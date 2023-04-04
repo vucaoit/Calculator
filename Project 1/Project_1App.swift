7a02360b4e6bd1e3e5c553782558de52d4993c3f
vucaoit <86138777+vucaoit@users.noreply.github.com>
2023-04-04 09:18:46 +0700

    Initial Commit

---
+++ Project 1/Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1App.swift
+//  Project 1
+//
+//  Created by vdcao on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
